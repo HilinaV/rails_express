b4e9b17129c8860f1666d264f13b7402969c4911
hilina <hilinahv@gmail.com>
2013-08-24 17:53:01 +0530

    Adding root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ RailsExpress::Application.routes.draw do
 
   resources :users
 
+  root :to => 'tweets#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
