4b7e38f53dbc51b3a0c76b11c392138c36d50f9d
hilina <hilinahv@gmail.com>
2013-08-24 15:48:38 +0530

    adding intial template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsExpress::Application.config.secret_key_base = '68dbc00f9e0c44f781210dcafc1dbad11b328606c98c379fb93681045c1135c2e94df9cd455a2d17aa5c7f7513f161d465f88eddda97ed4d7cbd2a2b30585190'
